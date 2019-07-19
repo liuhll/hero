d9569d569c34d511a8baca35d7cae8a3e3b376ea
liuhll <1029765111@qq.com>
2019-07-19 09:26:39 +0800

    更新字典表结构设计

--- hero/db/sql/basicdata.sql
+++ hero/db/sql/basicdata.sql
@@ -12,6 +12,8 @@ drop table if exists SystemConfig;
 
 drop table if exists Wordbook;
 
+drop table if exists WordbookItem;
+
 /*==============================================================*/
 /* Table: SystemConfig                                          */
 /*==============================================================*/
@@ -40,24 +42,41 @@ alter table SystemConfig comment 'SystemConfig';
 /*==============================================================*/
 create table Wordbook
 (
-   Id                   bigint not null auto_increment comment '主键',
-   Code                 varchar(50) not null comment '唯一编码',
+   Id                   bigint not null auto_increment,
+   Code                 varchar(50) not null comment '字典编码',
    Name                 varchar(50) not null comment '字典名称',
-   Value                varchar(50) not null comment '字典值',
-   ParentId             bigint not null comment '父级Id',
-   Seq                  int not null comment '序号',
-   TypeName             varchar(50) comment '分类名称',
-   HasChild             int not null comment '0.没有 1.有',
-   IsSysPreSet          int not null comment '0. 否 1.是',
-   CreateBy             bigint comment '创建人',
-   CreateTime           datetime comment '创建日期',
-   UpdateBy             bigint comment '修改人',
-   UpdateTime           datetime comment '修改日期',
-   IsDeleted            int comment '软删除标识',
+   Type                 int not null comment '1. 系统类型字典 2. 业务类型字典',
+   Memo                 varchar(100) comment '备注',
+   IsDeleted            int not null default 0 comment '软删除标识',
    DeleteBy             bigint comment '删除用户',
    DeleteTime           datetime comment '删除时间',
+   CreateBy             bigint comment '创建人',
+   CreateTime           date comment '创建时间',
+   UpdateBy             bigint comment '更新人',
+   UpdateTime           date comment '更新时间',
    primary key (Id)
 );
 
 alter table Wordbook comment '字典表';
 
+/*==============================================================*/
+/* Table: WordbookItem                                          */
+/*==============================================================*/
+create table WordbookItem
+(
+   Id                   bigint not null auto_increment comment '主键Id',
+   WordBookId           bigint not null comment '字典Id',
+   Key                  varchar(50) not null comment 'Key值',
+   Value                varchar(50) not null comment 'Value值',
+   Memo                 varchar(100) comment '备注',
+   IsDeleted            int not null default 0 comment '软删除标识',
+   DeleteBy             bigint comment '删除用户',
+   DeleteTime           datetime comment '删除时间',
+   CreateBy             bigint comment '创建人',
+   CreateTime           date comment '创建时间',
+   UpdateBy             bigint comment '更新人',
+   UpdateTime           date comment '更新时间',
+   primary key (Id)
+);
+
+alter table WordbookItem comment '字典项表';
