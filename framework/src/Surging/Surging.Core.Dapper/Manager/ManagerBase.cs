72f3d0e397d6b94c68b7f9b13a977c402e211660
liuhll <1029765111@qq.com>
2019-08-11 00:59:42 +0800

    优化ManagerBase基类

--- framework/src/Surging/Surging.Core.Dapper/Manager/ManagerBase.cs
+++ framework/src/Surging/Surging.Core.Dapper/Manager/ManagerBase.cs
@@ -1,8 +1,11 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Autofac;
+using MySql.Data.MySqlClient;
 using Oracle.ManagedDataAccess.Client;
+using Surging.Core.CPlatform.DependencyResolution;
 using Surging.Core.CPlatform.EventBus.Events;
 using Surging.Core.CPlatform.EventBus.Implementation;
 using Surging.Core.CPlatform.Utilities;
+using Surging.Core.ProxyGenerator;
 using System;
 using System.Data.Common;
 using System.Data.SqlClient;
@@ -12,6 +15,7 @@ namespace Surging.Core.Dapper.Manager
 {
     public abstract class ManagerBase 
     {
+ 
         protected virtual DbConnection Connection
         {
             get
@@ -80,22 +84,75 @@ namespace Surging.Core.Dapper.Manager
 
         public virtual T GetService<T>() where T : class
         {
-            return ServiceLocator.GetService<T>();
+            if (ServiceLocator.Current.IsRegistered<T>())
+            {
+                return ServiceLocator.GetService<T>();
+            }
+            else
+            {
+                var result = ServiceResolver.Current.GetService<T>();
+                if (result == null) {
+                    result = ServiceLocator.GetService<IServiceProxyFactory>().CreateProxy<T>();
+                    ServiceResolver.Current.Register(null,result);
+                }
+                return result;
+            }
+
+            
         }
 
         public virtual T GetService<T>(string key) where T : class
         {
-            return ServiceLocator.GetService<T>(key);
+            if (ServiceLocator.Current.IsRegisteredWithKey<T>(key))
+            {
+                return ServiceLocator.GetService<T>(key);
+            }
+            else
+            {
+                var result = ServiceResolver.Current.GetService<T>(key);
+                if (result == null)
+                {
+                    result = ServiceLocator.GetService<IServiceProxyFactory>().CreateProxy<T>(key);
+                    ServiceResolver.Current.Register(key, result);
+                }
+                return result;
+            }
         }
 
         public virtual object GetService(Type type)
         {
-            return ServiceLocator.GetService(type);
+            if (ServiceLocator.Current.IsRegistered(type))
+            {
+                return ServiceLocator.GetService(type);
+            }
+            else
+            {
+                var result = ServiceResolver.Current.GetService(type);
+                if (result == null)
+                {
+                    result = ServiceLocator.GetService<IServiceProxyFactory>().CreateProxy(type);
+                    ServiceResolver.Current.Register(null, result);
+                }
+                return result;
+            }
         }
 
         public virtual object GetService(string key, Type type)
         {
-            return ServiceLocator.GetService(key, type);
+            if (ServiceLocator.Current.IsRegisteredWithKey(key,type))
+            {
+                return ServiceLocator.GetService(key, type);
+            }
+            else
+            {
+                var result = ServiceResolver.Current.GetService(type);
+                if (result == null)
+                {
+                    result = ServiceLocator.GetService<IServiceProxyFactory>().CreateProxy(type);
+                    ServiceResolver.Current.Register(key, result);
+                }
+                return result;
+            }
         }
 
         public void Publish(IntegrationEvent @event)
