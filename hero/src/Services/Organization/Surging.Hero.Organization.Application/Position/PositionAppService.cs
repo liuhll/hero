b4da0a15a7458ad32e6a8c0912d1f183bcb8ad8a
liuhll <1029765111@qq.com>
2019-08-21 20:43:46 +0800

    实现通过主键Id获取职位信息接口

---
+++ hero/src/Services/Organization/Surging.Hero.Organization.Application/Position/PositionAppService.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Surging.Core.AutoMapper;
+using Surging.Core.Dapper.Repositories;
+using Surging.Core.ProxyGenerator;
+using Surging.Hero.Organization.Domain.Positions;
+using Surging.Hero.Organization.IApplication.Position;
+using Surging.Hero.Organization.IApplication.Position.Dtos;
+
+namespace Surging.Hero.Organization.Application.Position
+{
+    public class PositionAppService : ProxyServiceBase, IPositionAppService
+    {
+        private readonly IPositionDomainService _positionDomainService;
+        private readonly IDapperRepository<Domain.Positions.Position, long> _positionRepository;
+
+        public PositionAppService(IPositionDomainService positionDomainService,
+            IDapperRepository<Domain.Positions.Position, long> positionRepository) {
+            _positionDomainService = positionDomainService;
+            _positionRepository = positionRepository;
+        }
+
+        public async Task<GetPositionOutput> Get(long id)
+        {
+            var position = await _positionRepository.GetAsync(id);
+            return position.MapTo<GetPositionOutput>();
+        }
+    }
+}
---
+++ hero/src/Services/Organization/Surging.Hero.Organization.IApplication/Position/IPositionAppService.cs
@@ -0,0 +1,15 @@
+﻿using Surging.Core.CPlatform.Ioc;
+using Surging.Core.CPlatform.Runtime.Server.Implementation.ServiceDiscovery.Attributes;
+using Surging.Hero.Common;
+using Surging.Hero.Organization.IApplication.Position.Dtos;
+using System.Threading.Tasks;
+
+namespace Surging.Hero.Organization.IApplication.Position
+{
+    [ServiceBundle(HeroConstants.RouteTemplet)]
+    public interface IPositionAppService : IServiceKey
+    {
+        [ServiceRoute("{id}")]
+        Task<GetPositionOutput> Get(long id);
+    }
+}
