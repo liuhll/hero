08b49e61123bacae6f2cb5ba34300a3ea1077ca2
liuhll <1029765111@qq.com>
2019-09-09 23:03:24 +0800

    优化获取角色信息接口

---
+++ src/Services/Auth/Surging.Hero.Auth.Domain.Shared/Roles/RoleType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Surging.Hero.Auth.Domain.Shared.Roles
+{
+    public enum RoleType
+    {
+        [Description("通用")]
+        Universal = 0,
+
+        [Description("部门")]
+        Department
+    }
+}
--- src/Services/Auth/Surging.Hero.Auth.IApplication/Role/Dtos/GetRoleOutput.cs
+++ src/Services/Auth/Surging.Hero.Auth.IApplication/Role/Dtos/GetRoleOutput.cs
@@ -1,4 +1,5 @@
-﻿using Surging.Hero.Common;
+﻿using Surging.Hero.Auth.Domain.Shared.Roles;
+using Surging.Hero.Common;
 using Surging.Hero.Common.Extensions;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,20 @@ namespace Surging.Hero.Auth.IApplication.Role.Dtos
         public long Id { get; set; }
 
         public string DeptName { get; set; }
+
+        public RoleType RoleType {
+            get
+            {
+                if (!DeptId.HasValue || DeptId == 0) {
+                    return RoleType.Universal;
+                }
+                return RoleType.Department;
+            }
+        }
+
+        public string RoleTypeDesc {
+            get { return RoleType.GetDescription(); }
+        }
 
         public Status Status { get; set; }
 
