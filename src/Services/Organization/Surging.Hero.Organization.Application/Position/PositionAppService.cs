689cf45f7a14a4ee0c9db871322c0132a7b2aa60
liuhongliang <1029765111@qq.com>
2020-12-10 23:30:05 +0800

    优化通过OrgId获取部门信息接口

--- src/Services/Organization/Surging.Hero.Organization.Application/Position/PositionAppService.cs
+++ src/Services/Organization/Surging.Hero.Organization.Application/Position/PositionAppService.cs
@@ -8,6 +8,7 @@ using Surging.Core.ProxyGenerator;
 using Surging.Core.Validation.DataAnnotationValidation;
 using Surging.Hero.Auth.IApplication.User;
 using Surging.Hero.Organization.Domain.Positions;
+using Surging.Hero.Organization.Domain.Shared.Organizations;
 using Surging.Hero.Organization.IApplication.Position;
 using Surging.Hero.Organization.IApplication.Position.Dtos;
 
@@ -18,13 +19,17 @@ namespace Surging.Hero.Organization.Application.Position
         private readonly IPositionDomainService _positionDomainService;
         private readonly IDapperRepository<Domain.Positions.Position, long> _positionRepository;
         private readonly IDapperRepository<Domain.Department,long> _departmentRepository;
+        private readonly IDapperRepository<Domain.Organization, long> _organizationRepository;
 
         public PositionAppService(IPositionDomainService positionDomainService,
             IDapperRepository<Domain.Positions.Position, long> positionRepository,
-            IDapperRepository<Domain.Department, long> departmentRepository) {
+            IDapperRepository<Domain.Department, long> departmentRepository,
+            IDapperRepository<Domain.Organization, long> organizationRepository)
+        {
             _positionDomainService = positionDomainService;
             _positionRepository = positionRepository;
             _departmentRepository = departmentRepository;
+            _organizationRepository = organizationRepository;
         }
 
 
@@ -59,14 +64,29 @@ namespace Surging.Hero.Organization.Application.Position
 
         public async Task<IEnumerable<GetPositionOutput>> GetDeptPosition(long deptId)
         {
-            var positions = await _positionRepository.GetAllAsync(p=>p.DeptId == deptId);
+            var dept = await _departmentRepository.SingleOrDefaultAsync(p => p.OrgId == deptId);
+            if (dept == null)
+            {
+                throw new BusinessException($"不存在OrgId为{deptId}的部门信息");
+            }
+            var positions = await _positionRepository.GetAllAsync(p => p.DeptId == dept.Id);
             return positions.MapTo<IEnumerable<GetPositionOutput>>();
         }
 
         public async Task<IEnumerable<GetPositionOutput>> GetDeptPositionByOrgId(long orgId)
         {
+            var organization = await _organizationRepository.SingleOrDefaultAsync(p => p.Id == orgId);
+            if (organization == null) {
+               
+                throw new BusinessException($"不存在OrgId为{organization}的组织机构信息");
+            }
+            if (organization.OrgType == OrganizationType.Corporation) 
+            {
+                throw new BusinessException($"{organization.Name}不是部门类型,请重新选择");
+            }
             var dept = await _departmentRepository.SingleOrDefaultAsync(p => p.OrgId == orgId);
-            if (dept == null) {
+            if (dept == null)
+            {
                 throw new BusinessException($"不存在OrgId为{orgId}的部门信息");
             }
             var positions = await _positionRepository.GetAllAsync(p => p.DeptId == dept.Id);
--- src/Services/Organization/Surging.Hero.Organization.Domain.Shared/Organizations/OrganizationType.cs
+++ src/Services/Organization/Surging.Hero.Organization.Domain.Shared/Organizations/OrganizationType.cs
@@ -4,6 +4,7 @@ namespace Surging.Hero.Organization.Domain.Shared.Organizations
 {
     public enum OrganizationType
     {
+        [Description("公司")]
         Corporation = 0,
 
         [Description("部门")]
