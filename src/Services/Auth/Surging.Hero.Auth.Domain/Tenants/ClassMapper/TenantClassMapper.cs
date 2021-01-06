d8aeaa025d912e7a7c90cb1a3f6bc9f37793989a
刘洪亮 <liuhongliang@jingshonline.com>
2021-01-06 16:32:00 +0800

    新增实体与数据表的映射关系

---
+++ src/Services/Auth/Surging.Hero.Auth.Domain/Tenants/ClassMapper/TenantClassMapper.cs
@@ -0,0 +1,9 @@
+﻿using Surging.Hero.Common.ClassMapper;
+
+namespace Surging.Hero.Auth.Domain.Tenants.ClassMapper
+{
+    public class TenantClassMapper : HeroClassMapper<Tenant>
+    {
+        
+    }
+}
