633bb9cf66e39aa6e1f77dfadfbc43a658526aa4
liuhongliang <1029765111@qq.com>
2021-01-17 04:26:34 +0800

    升级surging.cloud组件版本号;更新构建主机的方法

--- src/Shared/CSharpScripts/Program.cs
+++ src/Shared/CSharpScripts/Program.cs
@@ -1,78 +1,40 @@
-using System;
-using Autofac;
-using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
 using Surging.Cloud.Caching.Configurations;
-using Surging.Cloud.Consul.Configurations;
 using Surging.Cloud.CPlatform;
 using Surging.Cloud.CPlatform.Configurations;
-using Surging.Cloud.CPlatform.Utilities;
 using Surging.Cloud.EventBusRabbitMQ.Configurations;
-using Surging.Cloud.ProxyGenerator;
-using Surging.Cloud.ServiceHosting;
-using Surging.Cloud.ServiceHosting.Internal.Implementation;
-using Surging.Cloud.System.Intercept;
 using Surging.Cloud.Zookeeper.Configurations;
-using SurgingConfig = Surging.Cloud.CPlatform.AppConfig;
 
 namespace Surging.Hero.ServiceHost
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private async static Task Main(string[] args)
         {
-            var host = new ServiceHostBuilder()
-                .RegisterServices(builder =>
-                {
-                    builder.AddMicroService(option =>
-                    {
-                        option.AddServiceRuntime()
-                            .AddClientProxy()
-                            .AddRelateServiceRuntime()
-                            .AddConfigurationWatch()
-                            .AddServiceEngine(typeof(SurgingServiceEngine))
-                            .AddClientIntercepted(typeof(CacheProviderInterceptor))
-                            ;
-
-                        builder.Register(p => new CPlatformContainer(ServiceLocator.Current));
-                    });
-                })
-                .ConfigureLogging(loggging =>
-                {
-                    loggging.AddConfiguration(
-                        AppConfig.GetSection("Logging"));
-                })
-                .UseServer(options => { })
-                .UseConsoleLifetime()
-                .Configure(build =>
+            await Host.CreateDefaultBuilder(args)
+                .RegisterMicroServices()
+                .ConfigureAppConfiguration((hostContext, configure) =>
                 {
 #if DEBUG
-                    build.AddCacheFile("${cachePath}|/app/configs/cacheSettings.json", false, true);
-                    build.AddCPlatformFile("${surgingPath}|/app/configs/surgingSettings.json", false, true); 
-                    build.AddEventBusFile("${eventBusPath}|/app/configs/eventBusSettings.json", optional: false);
-                    build.AddConsulFile("${consulPath}|/app/configs/consul.json", false, true);
-                    build.AddZookeeperFile("${zookeeperPath}|/app/configs/zookeeper.json", false, true);
-
-
+                    configure.AddCacheFile("${cachePath}|/app/configs/cacheSettings.json", optional: false, reloadOnChange: true);
+                    configure.AddCPlatformFile("${surgingPath}|/app/configs/surgingSettings.json", optional: false,
+                        reloadOnChange: true);
+                    configure.AddEventBusFile("${eventBusPath}|/app/configs/eventBusSettings.json", optional: false, reloadOnChange: true);
+                    // configure.AddConsulFile("${consulPath}|/app/configs/consul.json", optional: false, reloadOnChange: true);
+                    configure.AddZookeeperFile("${zookeeperPath}|/app/configs/zookeeper.json", optional: false,
+                        reloadOnChange: true);
 #else
-                    build.AddCacheFile("${cachePath}|configs/cacheSettings.json", optional: false, reloadOnChange: true);                      
-                    build.AddCPlatformFile("${surgingPath}|configs/surgingSettings.json", optional: false,reloadOnChange: true);                    
-                 //   build.AddEventBusFile("configs/eventBusSettings.json", optional: false);
-                    build.AddConsulFile("configs/consul.json", optional: false, reloadOnChange: true);
-                     build.AddZookeeperFile("${zookeeperPath}|configs/zookeeper.json", optional: false, reloadOnChange: true);
+                    configure.AddCacheFile("${cachePath}|configs/cacheSettings.json", optional: false, reloadOnChange: true);
+                    configure.AddCPlatformFile("${surgingPath}|configs/surgingSettings.json", optional: false, reloadOnChange: true);
+                    configure.AddEventBusFile("${eventBusPath}|configs/eventBusSettings.json", optional: false);
+                    configure.AddConsulFile("${consulPath}|configs/consul.json", optional: false, reloadOnChange: true);
+                    configure.AddZookeeperFile("${zookeeperPath}|configs/zookeeper.json", optional: false, reloadOnChange: true);
 #endif
                 })
-                .UseProxy()
-                .UseStartup<Startup>()
-                .Build();
-
-            using (host.Run())
-            {
-                Console.WriteLine($"服务主机启动成功{DateTime.Now}。");
-
-#if DEBUG
-                // Startup.InitActions();
-#endif
-            }
+                .UseServer()
+                .UseClient()
+                .Build().RunAsync();
         }
     }
 }
